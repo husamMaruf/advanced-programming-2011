749e422a21df9f6b2cb43bc0f6f747bfb18f19c0
dankilman <dankilman@a7c0e201-94a5-d3f3-62b2-47f0ba67a830>
2011-03-06 20:04:57 +0000

    Add methods Change structure

--- advanced-cpp-pa1/advanced-cpp-pa1/intArray_t.cpp
+++ advanced-cpp-pa1/advanced-cpp-pa1/intArray_t.cpp
@@ -1,37 +1,58 @@
-//#include "intArray_t.h"
-#include "stdafx.h"
-#include <iostream>
+#include "intArray_t.h"
+
 using namespace std;
 
-class intArray_t {
-public:
-	intArray_t(); //Default CTOR
-	intArray_t(int initCapacity);
-	~intArray_t();
-
-	int isEmpty() { return length == 0; }
-	int numOfItems() { return length; }
-	int capacity() { return cap; }
-	void insert(int* element);
-	int* first() { return intArray[0]; }
-	int* last() { return intArray[length-1]; }
-	int* find(int value);
-	int* remove(int value);
-	void removeAll();
-	int removeAndDelete(int value);
-	void removeAndDeleteAll();
-	int append(int index, int* element);
-	int prepend(int index, int* element);
-
-private:
-	int** intArray;
-	int cap;
-	int length;
-};
+intArray_t::intArray_t() {
+	intArray = new int*[CAPACITY_EXPAND_FACTOR];
+	cap = CAPACITY_EXPAND_FACTOR;
+	length = 0;
+}
 
 intArray_t::intArray_t(int initCapacity) {
 	intArray = new int*[initCapacity];
+	cap = initCapacity;
+	length = 0;
 }
 
+void intArray_t::expand(int addedCapacity) {
+	cap += addedCapacity;
+	int** newIntArray = new int*[cap];
+	for (int i=0; i<length; i++) {
+		newIntArray[i] = intArray[i];
+	}
+	delete[] intArray;
+	intArray = newIntArray;
+}
 
+int intArray_t::append(int index, int* element) {
+	return addElement(index, element, false);
+}
 
+int intArray_t::prepend(int index, int* element) {
+	return addElement(index, element, true);
+}
+
+int intArray_t::addElement(int index, int* element, bool isPrepend) {
+	if (index < 0 || index > length-1) {
+		return 0;
+	}
+	if (length == cap) {
+		expand(CAPACITY_EXPAND_FACTOR);
+	}
+	int location = isPrepend ? index : index + 1;
+	for (int i=length-1; i>=location; i--) {
+		intArray[i+1] = intArray[i];
+	}
+	intArray[location] = element;
+	length += 1;
+	return 1;
+}
+
+int* intArray_t::find(int value) {
+	for (int i=0; i<length; i++) {
+		if (*intArray[i] == value) {
+			return intArray[i];
+		}
+	}
+	return 0;
+}
