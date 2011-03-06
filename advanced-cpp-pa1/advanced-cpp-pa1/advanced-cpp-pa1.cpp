5987d08a4e30dc5fe9092fe13b56340e793fdb95
horomena <horomena@bezeqint.net>
2011-03-06 18:03:35 +0000

    Skeleton

--- advanced-cpp-pa1/advanced-cpp-pa1/advanced-cpp-pa1.cpp
+++ advanced-cpp-pa1/advanced-cpp-pa1/advanced-cpp-pa1.cpp
@@ -3,14 +3,51 @@
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int _tmain(int argc, _TCHAR* argv[]) {
 
-	cout << "Hello World!" << endl;
+	intArray_t::intArray_t arr;
 
-	int i;
+	while(1) {
+		cout << "\n| n | fst | lst | empty | ins | rmv | RmvAll |A|P| < | : ";
+		char c;
+		cin >> c;
 
-	cin >> i;
+		if (c!='n' && c!='f' && c!='l' && c!='e' && c!='i' && c!='r' && c!='R' && c!='<' && c!='A' && c!='P' )   
+			break;
+
+		switch (c) {
+			case 'n' : 
+				cout << arr.numOfItems() << endl; 
+				break;
+			case 'f' :  
+				cout << *arr.first() << endl;   
+				break;
+			case 'l' : 
+				cout << *arr.last() << endl;      
+				break;
+			case 'e' : 
+				cout << (arr.isEmpty() ? "Empty" : "No") << endl; 
+				break;
+			case 'i' :  
+				// insert
+				break;
+			case 'r' :  
+				// remove
+				break;
+			case 'R' :  
+				// remove all
+				break;
+			case 'A' :  
+				// append
+				break;
+			case 'P' : 
+				// prepend
+				break;
+			case '<' : 
+				// print array
+				break;
+		}
+	}
 
 	return 0;
 }
