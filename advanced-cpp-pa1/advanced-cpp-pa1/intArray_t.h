a7315feb24deff91a763d7c7763f579fa6f0d82b
horomena <horomena@bezeqint.net>
2011-03-09 18:55:57 +0000

    Added discussed changes, still need to handle incorrect input by user in test application.

--- advanced-cpp-pa1/advanced-cpp-pa1/intArray_t.h
+++ advanced-cpp-pa1/advanced-cpp-pa1/intArray_t.h
@@ -9,27 +9,27 @@ using namespace std;
 class intArray_t {
 public:
 	intArray_t();
-	intArray_t(int initCapacity);
-	intArray_t(const intArray_t &arr);
+	intArray_t(const int& initCapacity);
 	~intArray_t();
-
 	int isEmpty() const;
 	int numOfItems() const;
 	int capacity() const;
 	void insert(int* element);
 	int* first() const;
 	int* last() const;
-	int* find(int value) const;
-	int* remove(int value);
+	int* find(const int& value) const;
+	int* remove(const int& value);
 	void removeAll();
-	int removeAndDelete(int value);
+	int removeAndDelete(const int& value);
 	void removeAndDeleteAll();
-	int append(int index, int* element);
-	int prepend(int index, int* element);
+	int append(const int& index, int* element);
+	int prepend(const int& index, int* element);
 	ostream& printToStream(ostream& os) const;
 private:
-	void expand(int addedCapacity);
-	int addElement(int index, int* element, bool isPrepend);
+	intArray_t(const intArray_t&);
+	intArray_t& operator= (const intArray_t&);
+	void expand(const int& addedCapacity);
+	int addElement(const int& index, int* element, bool isPrepend);
 	int** intArray;
 	int cap;
 	int length;
