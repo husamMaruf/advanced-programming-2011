3014cac4401bf34491816f8cbf182f4067affe49
dankilman <dankilman@a7c0e201-94a5-d3f3-62b2-47f0ba67a830>
2011-03-30 23:22:36 +0000

    handle input on default CTOR

--- advanced-cpp-pa3/advanced-cpp-pa3/memPage_t.cpp
+++ advanced-cpp-pa3/advanced-cpp-pa3/memPage_t.cpp
@@ -2,7 +2,12 @@
 
 memPage_t::memPage_t() : 
 	pageSize(defaultPageSize), currentPosition(0), actualSize(0), 
-	previous(0), next(0), pageBuffer(new byte[pageSize]) { }
+	previous(0), next(0) {
+	if (pageSize <= 0) {
+		throw ILLEGAL_PAGE_SIZE;
+	}	
+	pageBuffer = new byte[pageSize];
+	}
 
 memPage_t::memPage_t(const int& _pageSize) throw(int) : 
 	pageSize(_pageSize), currentPosition(0), actualSize(0), 
