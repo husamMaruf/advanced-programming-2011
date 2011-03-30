159fedb7c872a6f385c736e71723163f8b1c66a3
dankilman <dankilman@a7c0e201-94a5-d3f3-62b2-47f0ba67a830>
2011-03-30 20:03:50 +0000

    add signatures to memPage_t.h

--- advanced-cpp-pa3/advanced-cpp-pa3/memPage_t.h
+++ advanced-cpp-pa3/advanced-cpp-pa3/memPage_t.h
@@ -1,7 +1,38 @@
 #pragma once
 
+//TODO: move to common
+typedef char byte;
+
 class memPage_t {
 public:
+
+	static const int DEFAULT_PAGE_SIZE = 1024;
+
 	memPage_t();
+	memPage_t(const int& pageSize);
+
 	~memPage_t();
+
+	void setPosition(const int& position);
+	const int& getPosition() const { return currentPosition; };
+	const bool& isEmpty() const;
+	const bool& isFull() const;
+	const int& getActualSize();
+	const int& getPageCapacity() const { return pageSize; };
+	
+	template<class T> const int& read(T& elem, const int& size, const int& position) const;
+	template<class T> const int& write(const T& elem, const int& size, const int& position);
+	template<class T> const int& read(T& elem, const int& size) const;
+	template<class T> const int& write(const T& elem, const int& size);
+	
+private:
+	memPage_t(const memPage_t& page);
+	const memPage_t& operator=(const memPage_t& page);
+
+	memPage_t* next;
+
+	byte pageBuffer[DEFAULT_PAGE_SIZE]; // TODO change and define in CTOR
+	int currentPosition;
+	int pageSize;
+
 };
