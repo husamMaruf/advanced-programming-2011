f93c05a4946694948ea7c6d858c61005114054f6
dankilman <dankilman@a7c0e201-94a5-d3f3-62b2-47f0ba67a830>
2011-03-30 22:22:07 +0000

    some updates

--- trunk/advanced-cpp-pa3/advanced-cpp-pa3/memPage_t.cpp
+++ trunk/advanced-cpp-pa3/advanced-cpp-pa3/memPage_t.cpp
@@ -1,14 +1,31 @@
 #include "memPage_t.h"
 
-memPage_t::memPage_t() {
+memPage_t::memPage_t() : 
+	pageSize(defaultPageSize), currentPosition(0), actualSize(0), 
+	previous(0), next(0), pageBuffer(new byte[pageSize]) { }
 
+memPage_t::memPage_t(const int& _pageSize) throw(int) : 
+	pageSize(_pageSize), currentPosition(0), actualSize(0), 
+	previous(0), next(0)  { 
+	if (pageSize <= 0) {
+		throw ILLEGAL_PAGE_SIZE;
+	}
+	pageBuffer = new byte[pageSize];
 }
 
 memPage_t::~memPage_t() {
-
+	delete pageBuffer;
 }
 
 int memPage_t::defaultPageSize = 1024;
+
+void memPage_t::setPosition(const int &position) throw(int) {
+	if (position > actualSize) {
+		throw ILLEGAL_POSITION;
+	}
+
+	currentPosition = position;
+}
 
 int main(void) {
 	return 0;
--- trunk/advanced-cpp-pa3/advanced-cpp-pa3/memPool_t.cpp
+++ trunk/advanced-cpp-pa3/advanced-cpp-pa3/memPool_t.cpp
@@ -1,13 +1,17 @@
 #include "memPool_t.h"
 
 memPool_t::~memPool_t() {
+	for_each(pages.rbegin(), pages.rend(), &deleteMemPagePtr);
+}
 
+void deleteMemPagePtr(memPage_t&  page) {
+	delete &page;
 }
 
 const memPage_t* memPool_t::getLastPage() const {
 	list<memPage_t>::iterator result = pages.end();
 	result--;
-	return *result;
+	return &(*result);
 }
 
 void memPool_t::createPages(const int& amount) {
