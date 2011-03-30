6296cdaaf4d0faf6999882bb591b6d54bda023ea
horomena <horomena@bezeqint.net>
2011-03-30 23:08:27 +0000

    fix

--- advanced-cpp-pa3/advanced-cpp-pa3/memPool_t.cpp
+++ advanced-cpp-pa3/advanced-cpp-pa3/memPool_t.cpp
@@ -6,20 +6,19 @@ memPool_t::~memPool_t() {
 	for_each(pages.rbegin(), pages.rend(), &deleteMemPagePtr);
 }
 
-void deleteMemPagePtr(memPage_t& page) {
-	delete &page;
+void deleteMemPagePtr(memPage_t* page) {
+	delete page;
 }
 
 const memPage_t* memPool_t::getLastPage() const {
-	list<memPage_t>::const_iterator result = pages.end();
+	list<memPage_t*>::const_iterator result = pages.end();
 	result--;
-	return &(*result);
+	return *result;
 }
 
 void memPool_t::createPages(const int& amount) {
 	for (int i=0; i<amount; i++) {
-		memPage_t* page = new memPage_t();
-		//pages.push_back(*page); //Doesn't work for some reason //DAN TELL ME IF YOU KNOW WHATS WRONG??
+		pages.push_back(new memPage_t());
 	}
 	capacity+= amount;
 }
