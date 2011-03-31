5ff21fcaabc5ff9aa7da380d37ec34447b77113f
dankilman <dankilman@a7c0e201-94a5-d3f3-62b2-47f0ba67a830>
2011-03-31 14:04:53 +0000

    Some updates

--- trunk/advanced-cpp-pa3/advanced-cpp-pa3/memPool_t.cpp
+++ trunk/advanced-cpp-pa3/advanced-cpp-pa3/memPool_t.cpp
@@ -14,9 +14,76 @@ const memPage_t* memPool_t::getLastPage() const {
 	return *result;
 }
 
-void memPool_t::createPages(const int& amount) {
-	for (int i=0; i<amount; i++) {
-		pages.push_back(new memPage_t());
+void memPool_t::createPages(const int& amount) throw(int) {
+
+	if (amount < 1) {
+		throw ILLEGAL_NUMBER_OF_PAGES;
 	}
-	capacity+= amount;
+
+	for (int i=0; i<amount; i++) {
+		memPage_t* newPage = new memPage_t();
+		if (getNumOfPages() > 0) {
+			memPage_t* lastPage = (memPage_t*)getLastPage();
+			lastPage->next = newPage;
+			newPage->previous = lastPage;
+		}
+		pages.push_back(newPage);
+		if (getNumOfPages() == 1) {
+			currentPageIter = pages.begin();
+		}
+	}
+
+	capacity += amount * getDefaultPageSize();
+}
+
+template<class T> int memPool_t::write(const T& elem, const int& size) {
+	if (getNumOfPages() == 0) {
+		createPages(1);
+	}
+	
+	memPage_t* currentPage = getCurrentPage();
+	int position = currentPage->getPosition();
+
+	int freeSpaceInCurrentPage = currentPage->getPageCapacity() - currentPage->getActualSize();
+	int writtenSize = 0;
+	while (writtenSize < size) {
+		int bytesToWriteInCurrentPage = min(freeSpaceInCurrentPage, size-writtenSize);
+		currentPage->write(*(&elem + writtenSize), bytesToWriteInCurrentPage, position);
+		writtenSize += bytesToWriteInCurrentPage;
+		if (writtenSize < size) {
+			currentPageIter++;
+			if (currentPageIter == pages.end()) {
+				createPages(1);
+				currentPageIter = getLastPage();
+			}
+			currentPage = (memPage_t*)getCurrentPage();
+			freeSpaceInCurrentPage = currentPage->getPageCapacity();
+			position = 0;
+		}
+	}
+
+	currentPosition += size;
+	actualSize = max(actualSize, currentPosition);
+}
+
+void memPool_t::setCurrentPosition(const int& position) throw(int) {
+	if (position < 0 || position > actualSize) {
+		throw ILLEGAL_POSITION;
+	}
+
+	currentPosition = 0;
+
+	list<memPage_t*>::iterator iter = pages.begin();
+	memPage_t* currentPage;
+	int currentPosition = 0;
+	for (currentPage = *iter; iter != pages.end(); currentPage = *iter++) {
+		if (currentPosition <= position && currentPosition + currentPage->getPageCapacity() >= position) {
+			break;
+		}
+		currentPosition += currentPage->getPageCapacity();
+	}
+
+	currentPageIter = iter;
+	currentPage->setPosition(position-currentPosition);
+
 }
