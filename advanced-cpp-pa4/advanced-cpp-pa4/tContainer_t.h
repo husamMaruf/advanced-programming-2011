dfd3cdff5009a6c102d7b6023a7ce0862d6ac2f7
horomena <horomena@bezeqint.net>
2011-04-19 17:33:13 +0000

    my part

--- advanced-cpp-pa4/advanced-cpp-pa4/tContainer_t.h
+++ advanced-cpp-pa4/advanced-cpp-pa4/tContainer_t.h
@@ -7,30 +7,53 @@ using namespace std;
 template <class T,class Container>
 class tContainer_t {
 public:
-	tContainer_t();	//TODO Alon
+	//tContainer_t(); //default CTOR
 	tContainer_t(const tContainer_t<T,Container>& tContainer); //TODO Dan
 	~tContainer_t(); //TODO Dan
 
-	const tContainer_t& operator=(const tContainer_t<T,Container>& tContainer); //TODO Alon
+	const tContainer_t& operator=(const tContainer_t<T,Container>& tContainer);
 	T* operator[](unsigned index) const; // retrieve[] //TODO Dan
-	const tContainer_t& operator+=(const tContainer_t<T,Container>& tContainer); //TODO Alon
+	const tContainer_t& operator+=(const tContainer_t<T,Container>& tContainer);
 
-	bool isEmpty() const; //TODO Alon
+	bool empty() const { return (c.empty()); }
 	int size() const;	//TODO Dan
-	void append(const T* element); //TODO Alon
-	T* getFirst() const; //TODO Dan
-	T* getLast() const; //TODO Dan
+	void append(const T* element) { c.push_back(element); }
+	T* front() const; //TODO Dan
+	T* back() const; //TODO Dan
 	T* find(const T& element) const; //TODO Dan
-	bool remove(const T& element); //TODO Alon
-	void removeAll(); //TODO Alon
+	bool remove(const T& element);
+	void removeAll() { c.clear(); }
 
 	typedef typename Container::iterator iter_t;
 
 private:
 	Container c;
+
+	const Container& getContainer() const { return c; }
 };
 
 ostream& operator<<(ostream& os, const tContainer_t<T,Container>&);
 
+bool tContainer_t::remove(const T& element) {
+	for (iter_t iter = c.begin(); iter != c.end(); c++) {
+		if (**iter == element) {
+			c.erase(iter);
+			return true;
+		}
+	}
+	return false;
+}
 
+const tContainer_t& operator=(const tContainer_t<T,Container>& tContainer) {
+	c = tContainer.getContainer();
+	return *this;
+}
+
+const tContainer_t& tContainer_t::operator+=(const tContainer_t<T,Container>& tContainer) {
+	while (!tContainer.empty()) {
+		c.append(tContainer.front());
+		tContainer.getContainer().pop_front();
+	}
+	return *this;
+}
 
