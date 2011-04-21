32cbc9e977d6ff539e483d773af0efc5b4460168
dankilman <dankilman@a7c0e201-94a5-d3f3-62b2-47f0ba67a830>
2011-04-21 11:41:24 +0000

    update test

--- advanced-cpp-pa4/advanced-cpp-pa4/tContainer_t.cpp
+++ advanced-cpp-pa4/advanced-cpp-pa4/tContainer_t.cpp
@@ -28,6 +28,48 @@ protected:
 	int value;
 };
 
+// sample class
+class DataContainer {
+public:
+	DataContainer() : id(0), ch('a') { }
+	DataContainer(int _id, char _ch) : id(_id), ch(_ch) { }
+	bool operator==(const DataContainer& other) {
+		return (id==other.id && ch==other.ch);
+	}
+	friend ostream& operator<<(ostream& os, const DataContainer& cont) {
+		return os << "DC(" << cont.id << ", " << cont.ch << ")";
+	}
+	friend istream& operator>>(istream& is, DataContainer& cont) {
+		int id;
+		char ch;
+		cout << "enter id:" << endl;
+		is >> id;
+		cout << "enter ch: " << endl;
+		is >> ch;
+		cont.id = id;
+		cont.ch = ch;
+		return is;
+	}
+private:
+	int id;
+	char ch;
+};
+
+// DataContainer implementation for Generator<DataContainer>
+class DataContainerGenerator : public Generator<DataContainer> {
+public:
+	DataContainerGenerator() : id(0), ch('a') { }
+	DataContainer operator()() {
+		int oldId = id;
+		char oldChar = ch;
+		id += 1;
+		ch = ch == 'z' ? 'a' : ch+1;
+		return DataContainer(oldId,oldChar);
+	}
+private:
+	int id;
+	char ch;
+};
 
 template<class T, class Container>
 void test(tContainer_t<T,Container>& cont, Generator<T>& generator) {
@@ -172,12 +214,30 @@ void test(tContainer_t<T,Container>& cont, Generator<T>& generator) {
 
 int main(int argc, char** argv) {
 
-	IntGenerator generator;
+	IntGenerator intGenerator;
 	tContainer_t<int,list<int*>> intListContainer;
 
-	test(intListContainer, generator);
+	DataContainerGenerator dcGenerator;
+	tContainer_t<DataContainer,deque<DataContainer*>> dcDeqContainer;
 
-	system("Pause");
+	char choice;
+
+	cout << "Choose a test: " << endl;
+	cout << "[1]: int with list" << endl;
+	cout << "[2]: DataContainer with deque" << endl;
+	cin >> choice;
+
+	switch(choice) {
+		case '1':
+			test(intListContainer, intGenerator);
+			break;
+		case '2':
+			test(dcDeqContainer, dcGenerator);
+			break;
+		default:
+			cout << "bad choice, better luck next time" << endl;
+
+	}
 
 	return 0;
 
